57009013f7d3dfc2da2877eea0484d43ac376298
Bruno Botelho <bruno.botelho.br@gmail.com>
2025-06-11 21:29:16 -0300

    Refactor code_execution function in vuln_examples.py to enhance security by commenting out dangerous exec call

--- vuln_examples.py
+++ vuln_examples.py
@@ -18,12 +18,7 @@ def full_ssrf() -> None:
 def code_execution(request):
     if request.method == 'POST':
         first_name = base64.decodestring(request.POST.get('first_name', ''))
-        #BAD -- Allow user to define code to be run.
-        exec("setname('%s')" % first_name)
-
-
-# def code_execution(request):
-#     if request.method == 'POST':
-#         first_name = base64.decodestring(request.POST.get('first_name', ''))
-#         #GOOD --Call code directly
-#         setname(first_name)
+        # #BAD -- Allow user to define code to be run.
+        # exec("setname('%s')" % first_name)
+        #GOOD --Call code directly
+        setname(first_name)
