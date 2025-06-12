46a1368cd29ee00b6b255b062fc66b6ff6fb52b9
Bruno Botelho <bruno.botelho.br@gmail.com>
2025-06-11 21:27:45 -0300

    Refactor code_execution function in vuln_examples.py to improve security by commenting out dangerous exec call

--- vuln_examples.py
+++ vuln_examples.py
@@ -14,3 +14,16 @@ def full_ssrf() -> None:
     # # GOOD: `subdomain` is controlled by the server.
     # subdomain: Literal['europe'] | Literal['world'] = "europe" if target == "EU" else "world"
     # resp = requests.get(url="https://" + subdomain + ".example.com/data/")
+    
+def code_execution(request):
+    if request.method == 'POST':
+        first_name = base64.decodestring(request.POST.get('first_name', ''))
+        #BAD -- Allow user to define code to be run.
+        exec("setname('%s')" % first_name)
+
+
+# def code_execution(request):
+#     if request.method == 'POST':
+#         first_name = base64.decodestring(request.POST.get('first_name', ''))
+#         #GOOD --Call code directly
+#         setname(first_name)
